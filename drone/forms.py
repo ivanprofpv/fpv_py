63cec53a49958dd9f70d82b25dddc5e058d13516
IvanProFPV <akvabanka@yandex.ru>
2023-09-13 21:33:59 +0400

    added validations fields title and content

--- drone/forms.py
+++ drone/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import *
 
@@ -13,3 +14,17 @@ class AddDroneForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-input'}),
             'content': forms.Textarea(attrs={'cols': 60, 'rows': 10})
         }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 200:
+            raise ValidationError('Длина превышает 200 символов!')
+
+        return title
+
+    def clean_content(self):
+        content = self.cleaned_data['content']
+        if len(content) > 50000:
+            raise ValidationError('Длина превышает 50000 символов!')
+
+        return content
